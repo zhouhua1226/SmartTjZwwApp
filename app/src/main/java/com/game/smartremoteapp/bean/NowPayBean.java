5acb99a064dd73bcf1da8cb5c0aa8e5fbad20e0e
chen <2207736332@qq.com>
2018-08-16 09:31:39 +0800

    Merge branch 'master' of D:\android\SmartTjZwwApp\SmartTjZwwApp with conflicts.

---
+++ app/src/main/java/com/game/smartremoteapp/bean/NowPayBean.java
@@ -0,0 +1,8 @@
+package com.game.smartremoteapp.bean;
+
+/**
+ * Created by mi on 2018/8/15.
+ */
+
+public class NowPayBean {
+}
---
+++ app/src/main/java/com/game/smartremoteapp/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,8 @@
+package com.game.smartremoteapp.wxapi;
+
+/**
+ * Created by mi on 2018/8/15.
+ */
+
+public class WXPayEntryActivity {
+}
